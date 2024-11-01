508eb4fcccd459c3439c7e8815241be386016a59
0x280 <86424319+0x280@users.noreply.github.com>
2024-11-01 16:31:59 +0100

    set initial root password empty

--- hosts/lila/users.nix
+++ hosts/lila/users.nix
@@ -5,15 +5,19 @@
   ...
 }:
  {
-  users.users.x280 = {
-    isNormalUser = true;
-    extraGroups = ["wheel"];
+  users.users = {
+    x280 = {
+      isNormalUser = true;
+      extraGroups = ["wheel"];
 
-    packages = with pkgs; [
-    ];
+      packages = with pkgs; [
+      ];
 
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL3UV6wQvHES2WuIVEAQf29RZVTihVNlfIEvweMmaAPs"
-    ];
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL3UV6wQvHES2WuIVEAQf29RZVTihVNlfIEvweMmaAPs"
+      ];
+    };
+
+    root.initialPassword = "";
   };
 }
