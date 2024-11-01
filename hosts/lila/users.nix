521b6b81aa086560257d839fbf2c2e72acf26c0a
0x280 <86424319+0x280@users.noreply.github.com>
2024-11-01 18:18:54 +0100

    meow

--- hosts/lila/users.nix
+++ hosts/lila/users.nix
@@ -5,6 +5,7 @@
   ...
 }:
  {
+  users.mutableUsers = false;
   users.users = {
     x280 = {
       isNormalUser = true;
@@ -17,9 +18,10 @@
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL3UV6wQvHES2WuIVEAQf29RZVTihVNlfIEvweMmaAPs"
       ];
 
-      initialPassword = "";
+      hashedPassword = "$6$oc8o51TZ02/JBuZH$Mxxf.9Gp4QZEnfOeIfIErfTSMv9POy.IQ4/IQbeS8OC467iN0b9wC2Crh8U0jhZQgUFdBaku8UiExB8vTwYaS/";
     };
 
-    root.initialPassword = "";
+    root.hashedPassword = "$6$kjX41u/Al2a52ME.$/Fx6HbSIQVQx1W5pMKkmi/gDeEpvkaPDSHq/DtDI6yJaR9kK6A09XSsOKHQxcUeBYBDwahPaFgf8qPlpZP8xJ0";
+    root.hashedPasswordFile = null;
   };
 }
