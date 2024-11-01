1c477a6deae673c380220c9eb6e13a67da46e837
0x280 <86424319+0x280@users.noreply.github.com>
2024-11-01 18:31:14 +0100

    specify empty default password

--- hosts/lila/users.nix
+++ hosts/lila/users.nix
@@ -17,9 +17,9 @@
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL3UV6wQvHES2WuIVEAQf29RZVTihVNlfIEvweMmaAPs"
       ];
 
-      hashedPassword = "$6$TSu8AqeryhOOBcKL$ky2IALrPDl9TvYW7JDECSGFas077vyiJNawCyjVxeCBg2ZYtHvx66eTzGFhNnxFI1lKJ/MWwVEdivjA.WdN.Q.";
+      initialPassword = "";
     };
 
-    root.hashedPassword = "$6$lrcmEeNULEneeYdM$J1FmMHKwnMNOoBHfViTq7QpxHW3OffHA50KNsh8duI9ArGToOSmqOpiiY.Ae2YVdI.xPKxX0TJnJq.msuObj00";
+    root.initialPassword = "";
   };
 }
